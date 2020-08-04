db3ec6b91c1d56c51be771dddde949b1ce4c972b
Blue Electric <be@leekcake.moe>
2020-08-04 21:07:28 +0900

    Add basic avalonia project

---
+++ TVDGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TVDGUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+    }
+}
---
+++ TVDGUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,50 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Utils;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace TVDGUI.Views
+{
+    public class MainWindow : Window
+    {
+        private TextBox MaxDownloadTextBox;
+        private TextBox MaxChunkTextBox;
+        private TextBox PathTextBox;
+
+        private Button FindPathButton;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            MaxDownloadTextBox = this.FindControl<TextBox>("MaxDownloadTextBox");
+            MaxChunkTextBox = this.FindControl<TextBox>("MaxChunkTextBox");
+            PathTextBox = this.FindControl<TextBox>("PathTextBox");
+
+            FindPathButton = this.FindControl<Button>("FindPathButton");
+            FindPathButton.Click += FindPathButton_Click;
+
+            /*
+            MaxDownloadTextBox.GetObservable(TextBox.TextProperty).Subscribe(text =>
+            {
+
+            });
+            */
+        }
+
+        private async void FindPathButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var dialog = new OpenFolderDialog();
+            var path = await dialog.ShowAsync(this);
+            PathTextBox.Text = path;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
